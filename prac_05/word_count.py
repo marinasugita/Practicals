e50930fbcdf2db0cd0f7d92ee912e4917cf17b2b
marinasugita <marina.sugita@my.jcu.edu.au>
2018-03-27 12:27:39 +1000

    Clean up prac_05 based on feedback

--- prac_05/word_count.py
+++ prac_05/word_count.py
@@ -3,5 +3,7 @@ word_count = {}
 for word in words:
     word_count[word] = word_count.get(word,0) + 1
 
+width = max(len(word) for word in word_count)
+
 for word, value in sorted(word_count.items()):
-    print("{} : {}".format(word, value))
+    print("{:{}} : {}".format(word, width, value))
