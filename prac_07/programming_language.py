20406a92e7d2b204fb4b787d7888072e04abf5df
Marina Sugita <marina.sugita@my.jcu.edu.au>
2018-04-10 12:58:07 +1000

    Add str method to return a string for programming_language in prac_07

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -11,6 +11,10 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                           self.year)
+
     def is_dynamic(self):
         if self.typing == "Dynamic":
             return True
