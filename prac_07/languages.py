023268024575d619d4199346aef2316ddc1b2248
Marina Sugita <marina.sugita@my.jcu.edu.au>
2018-04-10 12:56:41 +1000

    Create a program to use ProgrammingLanguage class in prac_07

---
+++ prac_07/languages.py
@@ -0,0 +1,16 @@
+"""CP1404 Practical - Client code to use the ProgrammingLanguage class."""
+
+from prac_07.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+
+main()
