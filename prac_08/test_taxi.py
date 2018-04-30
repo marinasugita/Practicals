400a7215b809f5f26ee59dd59e84edfe5e00e8cd
Marina <marina.sugita@my.jcu.edu.au>
2018-04-30 20:49:21 +1000

    Create a class variable in taxi class for prac_08

--- prac_08/test_taxi.py
+++ prac_08/test_taxi.py
@@ -5,7 +5,7 @@ Test for Taxi class.
 
 from prac_08.taxi import Taxi
 
-prius_1 = Taxi("Prius 1", 100, 1.23)
+prius_1 = Taxi("Prius 1", 100)
 prius_1.drive(40)
 print(prius_1)
 print("Current fare is ${}".format(prius_1.get_fare()))
