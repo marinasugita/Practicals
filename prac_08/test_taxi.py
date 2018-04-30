3f36767735f2956b6a0563f634660fec3f1eda43
Marina <marina.sugita@my.jcu.edu.au>
2018-04-30 22:10:56 +1000

    Reformat test for Taxi class

--- prac_08/test_taxi.py
+++ prac_08/test_taxi.py
@@ -5,12 +5,18 @@ Test for Taxi class.
 
 from prac_08.taxi import Taxi
 
-prius_1 = Taxi("Prius 1", 100)
-prius_1.drive(40)
-print(prius_1)
-print("Current fare is ${}".format(prius_1.get_fare()))
 
-prius_1.start_fare()
-prius_1.drive(100)
-print(prius_1)
-print("Current fare is ${}".format(prius_1.get_fare()))
+def main():
+    """Test Taxi."""
+    prius_1 = Taxi("Prius 1", 100)
+    prius_1.drive(40)
+    print(prius_1)
+    print("Current fare is ${}".format(prius_1.get_fare()))
+
+    prius_1.start_fare()
+    prius_1.drive(100)
+    print(prius_1)
+    print("Current fare is ${}".format(prius_1.get_fare()))
+
+
+main()
