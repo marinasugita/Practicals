7572a892990dfcf9c042f7939d7f2df9afe3d628
Marina <marina.sugita@my.jcu.edu.au>
2018-04-30 21:55:39 +1000

    Add Silver Service Taxi class for prac_08

---
+++ prac_08/silver_service_taaxi.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practical
+Silver Service Taxi class.
+"""
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """A version of a Taxi that includes fanciness."""
+
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Silver Service Taxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = self.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
+        return "{}, plus flagfall of ${}".format(super().__str__(), self.fanciness)
+
+    def get_fare(self):
+        """Return the price for the Silver Service Taxi trip."""
+        return super().get_fare() + self.flagfall
