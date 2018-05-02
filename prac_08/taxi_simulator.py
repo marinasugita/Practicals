f967a3be7520078a9407d5c76234bf0bdaf29b11
Marina Sugita <marina.sugita@my.jcu.edu.au>
2018-05-02 13:03:35 +1000

    WIP Client code for taxi simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -10,3 +10,22 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     taxis = [Taxi("Pruis", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
+    print(MENU)
+    menu_choice = input("").upper()
+    while menu_choice != "Q":
+        if menu_choice == "C":
+            for index, taxi in enumerate(taxis):
+                print("{} - {}".format(index, taxi))
+                taxi_choice = int(input("Choose taxi: "))
+        elif menu_choice == "D":
+            pass
+            distance = int(input("Drive how far? "))
+
+
+        else:
+            print("Invalid menu choice")
+        menu_choice = input("").upper()
+    print("Taxis are now:")
+
+
+main()
